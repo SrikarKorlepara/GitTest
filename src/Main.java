e8978c34f64bdb114d67e69feab6e0547d557102
Srikar Korlepara <srikar.k.v.20@gmail.com>
2023-12-13 13:15:04 +0530

    it doesnt matter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! its mee mario");
+
+        // it doesnt matter what you think man
     }
 }
