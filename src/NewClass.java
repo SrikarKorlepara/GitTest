d4d4e8554b5feaaa956f6d4d3330bf9047c7011e
Srikar Korlepara <38483342+SrikarKorlepara@users.noreply.github.com>
2023-12-16 14:31:50 +0530

    Update NewClass.java

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,5 +1,6 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("New GIT COMMIT Being created");
+        // adding more bs to this java code as comments
     }
 }
